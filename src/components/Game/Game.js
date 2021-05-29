c7765b66e2339ae9eac1f0e6311ee2ab052fcbc9
97250 <97250@DESKTOP-H14PI62>
2021-05-29 18:57:42 +0300

    tictactoe task

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,8 +6,6 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { useFormik } from "formik";
 import { validateWinStreak, validateRowSize } from "../../utils/formValidation";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 
 const Game = () => {
   const [isGameStarted, setGameIsStarted] = useState("");
