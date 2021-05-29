fcefcf8313d0f4f3feae3666aff1af9a1b71437b
97250 <97250@DESKTOP-H14PI62>
2021-05-29 18:28:06 +0300

    tictactoe task

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,6 +6,8 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { useFormik } from "formik";
 import { validateWinStreak, validateRowSize } from "../../utils/formValidation";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const Game = () => {
   const [isGameStarted, setGameIsStarted] = useState("");
@@ -35,63 +37,75 @@ const Game = () => {
   };
 
   return (
-    <div className="game-container">
-      {isGameStarted ? (
-        <Board
-          boardRowNumber={isGameStarted.rowSize}
-          winStreak={isGameStarted.winStreak}
-          handleGameStatus={handleGameStatus}
-        />
-      ) : (
-        <form onSubmit={formik.handleSubmit}>
-          <Card elevation={10}>
-            <div className="card-content">
-              <div className="card-input">
-                <TextField
-                  error={!!formik.errors.rowSize}
-                  helperText={formik.errors.rowSize}
-                  fullWidth={true}
-                  required
-                  type="number"
-                  label="Row size"
-                  name="rowSize"
-                  placeholder="Enter row size"
-                  onChange={formik.handleChange}
-                  value={formik.values.rowSize}
-                />
-              </div>
+    <div style={{ width: "100%" }}>
+      <h1
+        style={{
+          color: "white",
+          textAlign: "center",
+          alignItems: "center",
+          background: "#3f51b5",
+        }}
+      >
+        Tic tac toe
+      </h1>
+      <div className="game-container">
+        {isGameStarted ? (
+          <Board
+            boardRowNumber={isGameStarted.rowSize}
+            winStreak={isGameStarted.winStreak}
+            handleGameStatus={handleGameStatus}
+          />
+        ) : (
+          <form onSubmit={formik.handleSubmit}>
+            <Card elevation={10}>
+              <div className="card-content">
+                <div className="card-input">
+                  <TextField
+                    error={!!formik.errors.rowSize}
+                    helperText={formik.errors.rowSize}
+                    fullWidth={true}
+                    required
+                    type="number"
+                    label="Row size"
+                    name="rowSize"
+                    placeholder="Enter row size"
+                    onChange={formik.handleChange}
+                    value={formik.values.rowSize}
+                  />
+                </div>
 
-              <div className="card-input">
-                <TextField
-                  required
-                  error={!!formik.errors.winStreak}
-                  helperText={formik.errors.winStreak}
-                  fullWidth={true}
-                  type="number"
-                  label="Win streak"
-                  name="winStreak"
-                  placeholder="Enter streak for win"
-                  onChange={formik.handleChange}
-                  value={formik.values.winStreak}
-                />
-              </div>
+                <div className="card-input">
+                  <TextField
+                    required
+                    error={!!formik.errors.winStreak}
+                    helperText={formik.errors.winStreak}
+                    fullWidth={true}
+                    type="number"
+                    label="Win streak"
+                    name="winStreak"
+                    placeholder="Enter streak for win"
+                    onChange={formik.handleChange}
+                    value={formik.values.winStreak}
+                  />
+                </div>
 
-              <div className="card-input">
-                <Button
-                  style={{ marginTop: "10px" }}
-                  color="primary"
-                  type="submit"
-                  fullWidth={true}
-                  variant="contained"
-                  disabled={false}
-                >
-                  Start Game
-                </Button>
+                <div className="card-input">
+                  <Button
+                    style={{ marginTop: "10px" }}
+                    color="primary"
+                    type="submit"
+                    fullWidth={true}
+                    variant="contained"
+                    disabled={false}
+                  >
+                    Start Game
+                  </Button>
+                </div>
               </div>
-            </div>
-          </Card>
-        </form>
-      )}
+            </Card>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
